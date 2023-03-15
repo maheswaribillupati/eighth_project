bf064814929c966bf4fa7b4d177dcc38aaf55e99
billupati maheswari <bmaheswari474@gmail.com>
2023-03-15 13:01:49 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def rajesh(request):
+    return render(request,'rajesh.html')
+def mahendhra(request):
+    return render(request,'mahendhra.html')
